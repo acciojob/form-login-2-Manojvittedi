8447625d30b4bc89a6bc9dd5869443ed017e195c
Manojvittedi <153159811+Manojvittedi@users.noreply.github.com>
2026-04-28 20:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let formElement = document.getElementById("formalert");
+
+formElement.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	let formData = e.target;
+	alert(`First Name: ${formData["First Name"].value} Name: ${formData["Last Name"].value} Phone Number: ${formData["Phone Number"].value} Email ID: ${formData["Email ID"].value}`);
+	
+})
